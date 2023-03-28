b036983c61aeb0e3fa95eb7c82fc2809bdb615bd
Seth Burns <seth@sethburns.com>
2023-03-27 20:43:38 -0700

    Complete dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,35 @@
+function createDragon(name, riderName, temperment) {
+  dragon = {}
+    dragon.name = name
+    dragon.rider = riderName
+    dragon.temperment = temperment
+    dragon.timesEaten = 0
+    dragon.hungry = true
 
+  return dragon
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten++
+  if (dragon.timesEaten === 3) {
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+function findFireBreathers(dragons) {
+  return dragons.filter(function(dragon) {
+    return dragon.temperment === 'aggressive'
+  })
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
